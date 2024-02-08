0bb8ce339d23069212bf4606fb18cc44740cfcf1
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-02-08 08:34:19 -0500

    Add createGrid() pseudo code

---
+++ script.js
@@ -0,0 +1,10 @@
+function createGrid(size=16) {
+    /* Create a square size x size grid 
+    - declare variable to store a div
+    - for i = 0,...,size:
+        create a div element (i.e. on of the grid squares)
+        append the div to the container in the html
+      
+    */
+    return;
+}
