4c9ef3a1394f2cd11a72e97663dddf40ea7379be
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-02-08 20:23:41 -0500

    Complete grid creation implementation

--- script.js
+++ script.js
@@ -13,14 +13,20 @@ sliderInput.addEventListener("input", () => {
 updateSizeBtn.addEventListener("click", () => {
     const gridDim = parseInt(sliderInput.value);
     const squareLength = (100/gridDim).toString() + "%";
-
-    for (let i = 0; i < gridDim; ++i) {
+    // Clear previous grid
+    gridBox.textContent = "";
+    // Make new grid
+    for (let i = 0; i < gridDim**2; ++i) {
         gridBox.appendChild(createSquare(squareLength));
     };
 })
 
 function createSquare(squareLength) {
     const div = document.createElement("div");
-    div.setAttribute("style", `border: 1px solid black; width: ${squareLength}; height: ${squareLength};`);
+    div.setAttribute("style",
+    `border-right: 1px solid black;\
+    border-top: 1px solid black;\
+    width: ${squareLength}; \
+    height: ${squareLength};`);
     return div;
 }
